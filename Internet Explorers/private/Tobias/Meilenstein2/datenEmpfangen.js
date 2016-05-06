718f3b728482c8c2e3ab3df53d5b922ab623134c
tjuenemann <tobias.juenemann@stud.hs-mannheim.de>
2016-05-06 18:26:04 +0200

    Meilenstein2, Aufgabe3 fertig gestellt.

--- Internet Explorers/private/Tobias/Meilenstein2/datenEmpfangen.js
+++ Internet Explorers/private/Tobias/Meilenstein2/datenEmpfangen.js
@@ -7,18 +7,48 @@
  *   Lucas Kneis
  *   Tobias Juenemann
  * 
- * Last updated: 30. April 2016
+ * Last updated: 06. May 2016
  */
 
-function ladeTabelle() {
+function ladeTabelle(param) {
 	var xhr = new XMLHttpRequest();
-	xhr.open('GET', 'http://139.59.134.26/api/players');
+	xhr.open('GET', 'http://139.59.134.26/api/players' + param);
 	xhr.responseType = 'json';
 	xhr.onload = function(e) {
-		var data = xhr.response();
+		var data = xhr.response;
+		
 		if(data != null) {
-			//console.log(this.response);
-			alert(data);
+			// Alte Werte löschen
+			while(document.getElementById('players').rows.length > 2) {
+				document.getElementById('players').deleteRow(2);
+			}
+			
+			// Neue Werte schreiben
+			for(var a = 0; a < data.length; a++) {
+				var nummer = a + 2;
+				
+                var tr = document.getElementById('players').insertRow(nummer);
+                var tdName = document.createElement('td');
+                var tdVerein = document.createElement('td');
+                var tdCoach = document.createElement('td');
+				var tdPosition = document.createElement('td');
+				var tdNummer = document.createElement('td');
+				var tdJahr = document.createElement('td');
+				
+                tdName.innerHTML = data[a].name;
+				tdVerein.innerHTML = data[a].club;
+				tdCoach.innerHTML = data[a].coach;
+				tdPosition.innerHTML = data[a].position;
+				tdNummer.innerHTML = data[a].number;
+				tdJahr.innerHTML = data[a].year;
+                
+				tr.appendChild(tdName);
+				tr.appendChild(tdVerein);
+				tr.appendChild(tdCoach);
+				tr.appendChild(tdPosition);
+				tr.appendChild(tdNummer);
+				tr.appendChild(tdJahr);
+			}
 		}
 	};
 	xhr.send(null);
